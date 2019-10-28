8577285022d670ff0b0b0f1bb6878a555e191d10
nfriauf <54052466+nfriauf@users.noreply.github.com>
2019-10-27 21:34:34 -0500

    Ex2
    Ex2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        System.out.println("Enter string data");
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+
+        if (text.length() % 2 == 0) {
+            System.out.println("String is even");
+        }
+        else {
+            System.out.println("String is not even");
+        }
+
     }
 }
